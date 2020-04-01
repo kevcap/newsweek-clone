10ef4e536a2abfe3ccc6630e42f518c989578842
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-04-01 18:43:32 -0300

    update #25: add search bar responsive and fluid, add weather icon

--- js/weather.js
+++ js/weather.js
@@ -8,10 +8,13 @@ var day;
 var hour;
 
 var temp;
+var weather;
+var icon;
 
 function setItems() {
   //set items 
   $(".city-name").html(city + ', ');
+  $(".weather-icon").css("background-image", `url(${icon})`)
   $(".country-name").html(country);
   $(".temperature").html(temp + 'º');
   date = date.toString().split(' ');
@@ -43,10 +46,46 @@ async function getClientInfo() {
   //getWeather
   await $.getJSON(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9077cf2a8553d1a29a2c60fbf981f035`).then((dataWeather) => {
     temp = Math.ceil(dataWeather.main.temp - 273.15)  // convert it for clecius
+    weather = dataWeather.weather[0].main;
+
+    
+    if (weather == "Clear" && day == true ) {
+      icon = "https://g.newsweek.com/img/weather/32.png"
+      console.log('case1');
+    } else if ( weather == "Clear" && day === false) {
+      icon = "https://g.newsweek.com/img/weather/31.png"
+      console.log('case2');
+    } else if ( weather == "Clouds" && day === true) {
+      console.log('case3');
+      icon = "https://g.newsweek.com/img/weather/30.png"
+    } else if ( weather == "Clouds" && day === false ) {
+      console.log('case4');
+      icon = "https://g.newsweek.com/img/weather/27.png"
+    } else if ( weather == "Thunderstorm") {
+      console.log('case5');
+      icon = "https://g.newsweek.com/img/weather/03.png"
+    } else if ( weather == "Drizzle" ) {
+      console.log('case6');
+      icon = "https://openweathermap.org/img/wn/09d@2x.png"
+    } else if ( weather == "Rain" && day == true ) {
+      console.log('case7');
+      icon = "https://g.newsweek.com/img/weather/40.png"
+    } else if ( weather == "Rain" && day == false) {
+      console.log('case8');
+      icon = "https://g.newsweek.com/img/weather/45.png"
+    } else if ( weather == "Snow" ) {
+      console.log('case9');
+      icon = "https://g.newsweek.com/img/weather/14.png"
+    } else if ( weather == "Smoke" || "Haze" || "Fog" || "Dust" || "Sand" || "Ash" || "Tornado" || "Squall" || "Mist" ) {
+      console.log('case10');
+      icon = "https://openweathermap.org/img/wn/50d@2x.png"
+    } else {
+      icon = "https://g.newsweek.com/img/weather/30.png"
+      console.log('error'); 
+    }
   })
 
 
-  // async function getWeather () {
    
     console.log(`Log Ip- ${ip}`);
     console.log(`Log City- ${city}`);
@@ -55,6 +94,7 @@ async function getClientInfo() {
     console.log(`Log Day- ${day}`);
     console.log(`Log Hours- ${hour}`);
     console.log(`Log Temp- ${temp} º`);
+    console.log(`Log Weather- ${weather}`);
     setItems();
 };
 
